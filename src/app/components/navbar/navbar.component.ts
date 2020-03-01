aab2b60ee1ccb8bc09644465a44af1785ea98df1
Gabriel García Rodríguez <garcia.rodriguez.gabriel@gmail.com>
2020-03-01 21:45:46 +0100

    Add cover component

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -9,11 +9,11 @@ import { trigger, style, animate, transition } from '@angular/animations'
     trigger('appearMenu', [
       transition(':enter', [
         style({ transform: 'translateX(-100%)', opacity: 0 }),
-        animate('300ms', style({ transform: 'translateX(0)', opacity: 1 })),
+        animate('200ms', style({ transform: 'translateX(0)', opacity: 1 })),
       ]),
       transition(':leave', [
         style({ transform: 'translateX(0)', opacity: 1 }),
-        animate('300ms', style({ transform: 'translateX(-100%)', opacity: 0 })),
+        animate('200ms', style({ transform: 'translateX(-100%)', opacity: 0 })),
       ]),
     ]),
   ],
