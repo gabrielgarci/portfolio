d7609ef4ffd39c81bdc83551319e49d3bdb5ddb2
Gabriel García Rodríguez <garcia.rodriguez.gabriel@gmail.com>
2020-03-01 13:57:57 +0100

    Add navbar and give responsive functionalities (animated menu)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { NgModule } from '@angular/core'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+import { CoverComponent } from './components/cover/cover.component'
+import { NavbarComponent } from './components/navbar/navbar.component'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, CoverComponent, NavbarComponent],
+  imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,32 @@
+import { Component, HostListener, ElementRef } from '@angular/core'
+import { trigger, style, animate, transition } from '@angular/animations'
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+  animations: [
+    trigger('appearMenu', [
+      transition(':enter', [
+        style({ transform: 'translateX(-100%)', opacity: 0 }),
+        animate('300ms', style({ transform: 'translateX(0)', opacity: 1 })),
+      ]),
+      transition(':leave', [
+        style({ transform: 'translateX(0)', opacity: 1 }),
+        animate('300ms', style({ transform: 'translateX(-100%)', opacity: 0 })),
+      ]),
+    ]),
+  ],
+})
+export class NavbarComponent {
+  public showMenu = false
+
+  constructor(private eRef: ElementRef) {}
+
+  @HostListener('document:mousedown', ['$event'])
+  onGlobalClick(event): void {
+    if (!this.eRef.nativeElement.contains(event.target)) {
+      this.showMenu = false
+    }
+  }
+}
