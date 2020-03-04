e567b29812c24b44d02fd73b5ca4cf45483181f1
Gabriel García Rodríguez <garcia.rodriguez.gabriel@gmail.com>
2020-03-04 20:23:07 +0100

    Add animation to cover title

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -8,12 +8,12 @@ import { trigger, style, animate, transition } from '@angular/animations'
   animations: [
     trigger('appearMenu', [
       transition(':enter', [
-        style({ transform: 'translateX(-100%)', opacity: 0 }),
-        animate('200ms', style({ transform: 'translateX(0)', opacity: 1 })),
+        style({ transform: 'translateX(100%)', opacity: 0 }),
+        animate('350ms', style({ transform: 'translateX(0)', opacity: 1 })),
       ]),
       transition(':leave', [
         style({ transform: 'translateX(0)', opacity: 1 }),
-        animate('200ms', style({ transform: 'translateX(-100%)', opacity: 0 })),
+        animate('350ms', style({ transform: 'translateX(100%)', opacity: 0 })),
       ]),
     ]),
   ],
