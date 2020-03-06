d8b367bc7a4417168d0738d7ffdbe11505376c7a
Gabriel García Rodríguez <garcia.rodriguez.gabriel@gmail.com>
2020-03-06 19:29:23 +0100

    Create about structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { NgModule } from '@angular/core'
 import { AppComponent } from './app.component'
 import { CoverComponent } from './components/cover/cover.component'
 import { NavbarComponent } from './components/navbar/navbar.component'
+import { AboutComponent } from './components/about/about.component'
 
 @NgModule({
-  declarations: [AppComponent, CoverComponent, NavbarComponent],
+  declarations: [AppComponent, CoverComponent, NavbarComponent, AboutComponent],
   imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -6,6 +6,15 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent implements OnInit {
+  public skills = [
+    { name: 'CSS', score: 80 },
+    { name: 'JS', score: 85 },
+    { name: 'Angular', score: 80 },
+    { name: 'Ionic', score: 60 },
+    { name: 'SASS', score: 68 },
+    { name: 'Node.js', score: 55 },
+  ]
+
   constructor() {}
 
   ngOnInit(): void {}
