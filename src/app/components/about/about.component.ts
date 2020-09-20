bf92a5b64737029a53d4f2ab1fa14ae1cdc9855b
Gabriel Garcia <garcia.rodriguez.gabriel@gmail.com>
2020-09-20 13:26:29 +0200

    refactor app common styles

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core'
+import { Component } from '@angular/core'
 import { Skill, Value } from 'src/app/models/about'
 import { IntersectionDirective } from 'src/app/directives/intersection.directive'
 
